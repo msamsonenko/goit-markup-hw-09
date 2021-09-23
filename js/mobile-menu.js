f5d10cb71980a0f3ae29e8c719a1749aaa1cace7
samsonenkomax87 <samsonenko.max@gmail.com>
2021-09-23 16:29:41 +0300

    changes to header

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -7,7 +7,7 @@
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-hidden');
+    mobileMenu.classList.toggle('is-open');
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
@@ -22,7 +22,7 @@
   // в случае изменения ориентации устройства.
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
-    mobileMenu.classList.remove('is-hidden');
+    mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
